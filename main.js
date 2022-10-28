a8fd93985336c6e707f2759b18a3913d198e729b
Alex Damian Elgueta Rivas <alexelgueta.developer@gmail.com>
2022-10-28 13:12:52 -0300

    agregue %

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function showQuote(data) {
     <p class="main-price">Precio: <span>${PRICE}</span></p>
     <p>Precio más alto del día:: <span>${HIGHDAY}</span></p>
     <p>Precio más bajo del día: <span>${LOWDAY}</span></p>
-    <p>Variación últimas 24 horas: <span>${CHANGEPCT24HOUR}</span></p>
+    <p>Variación últimas 24 horas: <span>${CHANGEPCT24HOUR}%</span></p>
     <p>Última Actualización: <span>${LASTUPDATE}</span></p>
   `;
   containerAnswer.appendChild(answer);
